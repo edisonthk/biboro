b0cc6e3d6bfbd2833e5febccf11ff49203aff2d9
edisonthk-mac <edisonthk@gmail.com>
2015-02-17 23:01:51 +0900

    editorページを追加

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -1,18 +1,24 @@
+// React modules
 var React = require('react');
 var Router = require('react-router');
 var { Route, DefaultRoute, RouteHandler, Link } = Router;
 
+// Application modules
 var SnippetStore = require('./stores/SnippetStore');
-
 var ShortcutHandler = require('./components/ShortcutHandler');
 var SnippetList = require('./components/SnippetList');
 var SnippetDetail = require('./components/SnippetDetail');
 var Searchbox = require('./components/Searchbox');
+var SnippetEditor = require('./components/SnippetEditor');
+var SnippetConstants = require('./constants/SnippetConstants');
 
+// App class
+// The main of the whole codegarage application is handle here
 var App = React.createClass({
   render: function () {
     return (
       <div className="app">
+        <ShortcutHandler />
         <div className="nav">
           <div className="brand-name">
             <Link to="snippets">Home</Link>
@@ -20,7 +26,7 @@ var App = React.createClass({
           <div className="nav-field">
             <Searchbox placeholder="Search ..." />  
             <ul>
-              <li><Link to="dashboard">Dashboaffffffrd</Link></li>
+              <li><Link to="editor" params={{type: 'new'}}>新規作成</Link></li>
               <li><Link to="snippets">Home</Link></li>
             </ul>
           </div>
@@ -44,50 +50,43 @@ var Snippets = React.createClass({
     }
 });
 
+// var Form = React.createClass({
 
-var Dashboard = React.createClass({
-  render: function () {
-    return <h1>Dashboard</h1>;
-  }
-});
+//   mixins: [ Router.Navigation ],
 
-var Form = React.createClass({
+//   statics: {
+//     willTransitionFrom: function (transition, element) {
+//       if (element.refs.userInput.getDOMNode().value !== '') {
+//         if (!confirm('You have unsaved information, are you sure you want to leave this page?')) {
+//           transition.abort();
+//         }
+//       }
+//     }
+//   },
 
-  mixins: [ Router.Navigation ],
+//   handleSubmit: function (event) {
+//     event.preventDefault();
+//     this.refs.userInput.getDOMNode().value = '';
+//     this.transitionTo('/');
+//   },
 
-  statics: {
-    willTransitionFrom: function (transition, element) {
-      if (element.refs.userInput.getDOMNode().value !== '') {
-        if (!confirm('You have unsaved information, are you sure you want to leave this page?')) {
-          transition.abort();
-        }
-      }
-    }
-  },
-
-  handleSubmit: function (event) {
-    event.preventDefault();
-    this.refs.userInput.getDOMNode().value = '';
-    this.transitionTo('/');
-  },
-
-  render: function () {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <p>Click the dashboard link with text in the input.</p>
-          <input type="text" ref="userInput" defaultValue="ohai" />
-          <button type="submit">Go</button>
-        </form>
-      </div>
-    );
-  }
-});
+//   render: function () {
+//     return (
+//       <div>
+//         <form onSubmit={this.handleSubmit}>
+//           <p>Click the dashboard link with text in the input.</p>
+//           <input type="text" ref="userInput" defaultValue="ohai" />
+//           <button type="submit">Go</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// });
 
 var routes = (
   <Route handler={App}>
     <DefaultRoute handler={Snippets}/>
-    <Route name="dashboard" handler={Dashboard}/>
+    <Route name="editor" handler={SnippetEditor} />
     <Route name="snippets" handler={Snippets}>
       <Route name="snippet" path="/snippet/:id" handler={SnippetDetail} />
     </Route>
@@ -97,7 +96,6 @@ var routes = (
 Router.run(routes, function (Handler) {
   React.render(
     <div>
-      <ShortcutHandler />
       <Handler/>
     </div>
   , document.getElementById('example'));
--- resources/assets/components/ShortcutHandler.js
+++ resources/assets/components/ShortcutHandler.js
@@ -2,24 +2,39 @@
 // This components is not used as view, it is used as Handler
 
 var React = require('react');
+var Router = require('react-router');
+var { Route, DefaultRoute, RouteHandler, Link } = Router;
+
+
 var KeyConstants = require('../constants/KeyConstants');
 
 module.exports = React.createClass({
+
+	mixins: [ Router.Navigation, Router.State ],
+
 	componentDidMount: function() {
-		window.addEventListener('keyup', this.shortcutHandle,false);
+		window.addEventListener('keyup', this.shortcutHandler,false);
 	},
 	componentDidUnmount: function(){
-		window.removeEventListener('keyup', this.shortcutHandle);
+		window.removeEventListener('keyup', this.shortcutHandler);
 	},
-	shortcutHandle: function(e) {
+	shortcutHandler: function(e) {
 		var kwElement = document.getElementById("keywords");
 		var keyPressed = e.keyCode;
 
-		if( (keyPressed >= KeyConstants.KEY_0 && keyPressed <= KeyConstants.KEY_9) || 
-			( !(e.ctrlKey || e.metaKey) && keyPressed >= KeyConstants.KEY_A && keyPressed <= KeyConstants.KEY_Z )
-			|| keyPressed == 219 || keyPressed == 221 ){
-			kwElement.focus();
+		if(this.getRoutes()[1].name === 'editor'){
+			// editor shortcut key handle
+
+		}else{
+			// snippets shortuct key handle
+			if( (keyPressed >= KeyConstants.KEY_0 && keyPressed <= KeyConstants.KEY_9) || 
+				( !(e.ctrlKey || e.metaKey) && keyPressed >= KeyConstants.KEY_A && keyPressed <= KeyConstants.KEY_Z )
+				|| keyPressed == 219 || keyPressed == 221 ){
+				kwElement.focus();
+			}
 		}
+		
+		
 		// console.log("KEY : "+event.keyCode);
 	},
 	render: function() {
