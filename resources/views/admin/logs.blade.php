4b6bcbf21f197ace9c43730be87bc0c0f93b1912
edisonthk-mac <edisonthk@gmail.com>
2015-05-23 17:06:30 +0900

    add sortable table to log page

--- resources/views/admin/logs.blade.php
+++ resources/views/admin/logs.blade.php
@@ -5,6 +5,13 @@
   	<title></title>
   	<meta name="description" content="">
   	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+
+  	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"></style>
+	<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 	<div class="container">
@@ -15,13 +22,13 @@
 		</div>
 		<div class="row">
 			<div class="col-xs-12">
-				<table class="table">
+				<table class="table" style="width: 100%; max-width: 100%;">
 					<thead>
 						<tr>
-							<th>日付</th>
+							<th style="width: 200px;">日付</th>
 							<th>OS</th>
 							<th>ブラウザ</th>
-							<th>キーワード</th>
+							<th style="width: 600px;">キーワード</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -38,5 +45,10 @@
 			</div>
 		</div>
 	</div>
+	<script>
+	
+	$('.table').dataTable();
+	
+	</script>
 </body>
 </html>
