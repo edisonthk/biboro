cd588c86940fcef26f24a3a7806f3e258e46b178
edisonthk-mac <edisonthk@gmail.com>
2015-06-18 16:32:07 +0900

    handle tags

--- app/Snippet.php
+++ app/Snippet.php
@@ -1,5 +1,7 @@
 <?php namespace App;
 
+use App\Tag;
+use App\Snippet;
 
 
 class Snippet extends \Eloquent {
@@ -29,26 +31,21 @@ class Snippet extends \Eloquent {
 	//$id,$name,$tag_id,$snippet_id
 	public function tagsave($tags){
 
-		$created_date = date('Y-m-d G:i:s');
-		
-
 		$snippet_id = $this->id;
 		
-		\DB::delete('delete from snippet_tag where snippet_id = ?',array($snippet_id));
-		
-		
+        $tagsId = []; 
 		foreach($tags as $tag_name){
-			$db_tag = \DB::select("select id from tag_table where name=?",array($tag_name));
-			
+            $tag = Tag::where("name","=",$tag_name)->first();
+            if(is_null($tag)) {
+                $tag = new Tag;
+                $tag->name = $tag_name;
+                $tag->save();
+            }
 
-			if(count($db_tag) == 0){
-				$tag_id = \DB::table('tag_table')->insertGetId(array("name"=>$tag_name,"created_at"=>$created_date,"updated_at"=>$created_date));
-			}else{
-				$tag_id = $db_tag[0]->id;
-			}
-			\DB::insert("insert into snippet_tag (tag_id,snippet_id,created_at,updated_at) values (?,?,?,?)",array($tag_id,$snippet_id,$created_date,$created_date));
+            $tagsId[] = $tag->id;
 		}
 		
+        Snippet::find($snippet_id)->tags()->sync($tagsId);
 	}
 
 	public function getUpdatedAtInReadableFormat()
